a5d6741173729528dd13c525dcc8ed34c03a0d29
Fiovalds <70115200+Fiovalds@users.noreply.github.com>
2024-06-06 00:54:05 -0500

    Add files via upload

---
+++ map1.py
@@ -0,0 +1,9 @@
+# map1.py
+import sys
+
+for linea in sys.stdin:
+    claves = linea.strip().split(';')
+    if len(claves) > 3 and claves[3].isdigit():
+        marca = claves[0]
+        nasientos = int(claves[3])
+        print(f"{marca}\t{nasientos}")
